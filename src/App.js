4a4ec881d909321e8459a7af6ecd9316dc661e66
Pratiksha Shende <pratiksha.shende@ril.com>
2024-07-08 19:19:11 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DateCounter />
     </div>
   );
 }
 
-export default App;
+function DateCounter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  function handleRest() {
+    setCount(0);
+    setStep(1);
+  }
+  const date = new Date();
+  date.setDate = date.getDate() + count;
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>step:{step}</span>
+      </div>
+      <button onClick={() => setCount((c) => c - step)}>-</button>
+      <input
+        type="text"
+        value={count}
+        onChange={(e) => setCount(Number(e.target.value))}
+      />
+      <button onClick={() => setCount((c) => c + step)}>+</button>
+
+      <div>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was`}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleRest}>Reset</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
